c816d8ee7dee453d9b9a1e076c391c67977f6486
rozanmhzn <rozanmhzn07@gmail.com>
2024-02-13 12:38:35 +0545

    [feature]: signup page without background

--- frontend/ghardera/.next/server/server-reference-manifest.js
+++ frontend/ghardera/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/yLT9WmRyzPIXUJfBWcwMr75a+QRovKkrpEffduv8sw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wEnn8NKXzWNJkTd9CWE04/aZPjgCuxfrP2ZIC0241NE=\"\n}"
