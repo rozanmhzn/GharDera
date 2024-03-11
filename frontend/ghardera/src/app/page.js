cef3ddfdfa31f08bd97f273eb34a484a286654ca
rozanmhzn <rozanmhzn07@gmail.com>
2024-03-11 23:12:31 +0545

    [feature]: Hero-Section added on landing page

--- frontend/ghardera/src/app/page.js
+++ frontend/ghardera/src/app/page.js
@@ -1,11 +1,15 @@
-import Navbar from "./components/partials/Navbar";
+"use client";
+import HeroSection from "@/components/modules/Landing/HeroSection";
+import Navbar from "@/components/partials/Navbar";
 
 export default function Home() {
   return (
-    <>
-    <Navbar/>
-    <h1>Landing Page</h1>
-  
-    </>
+    <div>
+      {/*Main Body*/}
+      <main>
+        <Navbar/>
+        <HeroSection />
+      </main>
+    </div>
   );
 }
---
+++ frontend/ghardera/src/components/modules/Landing/HeroSection.jsx
@@ -0,0 +1,27 @@
+
+import Image from "next/image";
+import img from "../../../assets/heroSectionImage.svg";
+import SearchBar from "@/components/Searchbar/searchbar";
+
+const HeroSection = () => {
+  return (
+    <section className="relative h-[35rem] mt-4">
+      <div>
+        <Image src={img} alt="hero-image" layout="fill" objectFit="cover" />
+        <section>
+          <div className="overlay absolute inset-0 flex flex-col items-center justify-center text-white">
+            <h1 className="text-[64px] leading-[78px] font-bold mb-1 w-[40%] text-center ">
+              Find Your Perfect Place with Us{" "}
+            </h1>
+            <p className="text-xl mb-6">
+              Discover warmth and trust in every transaction.
+            </p>
+            <SearchBar />
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
