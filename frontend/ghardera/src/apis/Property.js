d3f09dbf69cb4645c057a8a00322ea19f579c941
rozanmhzn <rozanmhzn07@gmail.com>
2024-04-22 22:53:13 +0545

    [feature]: Search filter updated on frontend

--- frontend/ghardera/src/apis/Property.js
+++ frontend/ghardera/src/apis/Property.js
@@ -3,6 +3,10 @@ import {GetRequest,DeleteRequest, PostRequest, PutRequest} from "@/plugins/https
 
 export const APIGetAllProperty = () => GetRequest("property/user/properties/");
 
+export const APIGetParamsProperty = (query) =>  GetRequest(`property/search?${query}`);
+
+export const APIGetEachProperty = (slug) => GetRequest(`property/admin/properties/${slug}`);
+
 export const APIAddProperty = (data) => PostRequest("property/admin/property", data);
 
 export const APIDeleteProperty = (slug) => DeleteRequest(`property/admin/property/${slug}`);
