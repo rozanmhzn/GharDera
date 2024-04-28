76ffa5e3703d4c7b086687633ae17aac356f8d5b
rozanmhzn <rozanmhzn07@gmail.com>
2024-04-28 23:03:44 +0545

    [feature]: 2fa UI added on frontend

--- frontend/ghardera/src/apis/Auth.js
+++ frontend/ghardera/src/apis/Auth.js
@@ -3,3 +3,6 @@ const { GetRequest, PostRequest } = require("@/plugins/https");
 export const APILoginUser = (data) => PostRequest("user/login", data);
 export const APIUserDetail = () => GetRequest("user/user/profile");
 export const APIUserRegister = (data) => PostRequest("user/signup", data);
+export const APIVerifyUser = (data, token) =>PostRequest(`auth/verifyuser/${token}`, data);
+export const APIVerifyOTP = (data) => PostRequest(`auth/verifyotp`, data);
+export const APIResendOTP = (id) => PostRequest(`auth/resend-otp/${id}`);
