72c9c972b22718a8e522aa0e9eeafc129df33b62
rozanmhzn <rozanmhzn07@gmail.com>
2024-05-02 22:58:17 +0545

    [feature]: Dashboard page on Admin panel

--- frontend/ghardera/src/apis/User.js
+++ frontend/ghardera/src/apis/User.js
@@ -1,5 +1,9 @@
-const { GetRequest } = require("@/plugins/https");
+const { GetRequest, PutRequest } = require("@/plugins/https");
 
 export const APIGetAllUsers = () => GetRequest("user/admin/users");
 
 export const APIGetUserBooking = () => GetRequest("user/user/bookings");
+
+export const APIGetAllBookings = () => GetRequest(`user/admin/bookings`);
+
+export const APIChangePasswordAdmin = (data) =>  PutRequest(`user/admin/settings/changepassword`, data);
