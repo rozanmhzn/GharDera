e98a290cda01b397e399290eb403700e362ebf36
rozanmhzn <rozanmhzn07@gmail.com>
2024-05-24 13:49:52 +0545

    [feature]: About us Page

---
+++ frontend/ghardera/src/app/(routes)/about/page.jsx
@@ -0,0 +1,39 @@
+export default function About() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+        <div className="w-full max-w-4xl px-5">
+          <div className="text-4xl font-bold text-center text-blue-900 ">
+            About Us
+          </div>
+          <div className="text-lg font-medium text-gray-800 p-6 bg-white shadow-md rounded-lg">
+            <p className="mb-6">
+              Welcome to Ghardera, your premier destination for finding your
+              dream home. At Ghardera, we believe that the process of buying,
+              selling, or renting a property should be as smooth and enjoyable
+              as possible. Our mission is to provide a user-friendly platform
+              that connects buyers, sellers, and renters with the best real
+              estate options available. Whether you're looking for a cozy
+              apartment in the city, a spacious family home in the suburbs, or a
+              luxurious villa by the beach, Ghardera has you covered. Our
+              extensive listings, detailed property descriptions, and
+              high-quality images ensure that you have all the information you
+              need to make informed decisions.
+            </p>
+            <p>
+              At Ghardera, we are passionate about real estate and committed to
+              excellence. Our team of experienced professionals is dedicated to
+              helping you navigate the complexities of the real estate market
+              with ease. We value transparency, integrity, and customer
+              satisfaction above all else. By leveraging the latest technology
+              and market insights, we strive to provide an unparalleled user
+              experience. Join us on this exciting journey and discover why
+              Ghardera is the trusted name in real estate. Your dream home is
+              just a click away!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
